0fa68273d2a2d470f3dd6792b3f0d258b9fc8721
Tristan Smith <tristan@shortcord.com>
2016-08-27 11:22:36 -0400

    more stuff

---
+++ js/trianglify.colors.js
@@ -0,0 +1,8 @@
+var TrianglifyColors = function () { };
+
+TrianglifyColors.PrincessToQueen = ["#FAAE8A", "#DC8574", "#AF5D6B", "#6C3755", "#432142"]; //http://www.colourlovers.com/palette/3806262/Princess_to_Queen
+TrianglifyColors.CrashingOceanWaves = ["#2A4347", "#4A7D6A", "#648D7D", "#ADB4AC", "#DEDFD9"]; //http://www.colourlovers.com/palette/2331840/Crashing_Ocean_Waves
+
+TrianglifyColors.prototype.RainbowColorFunction = function(x, y) {
+	return 'hsl('+Math.floor(Math.abs(x*y)*360)+',80%,60%)';
+};
